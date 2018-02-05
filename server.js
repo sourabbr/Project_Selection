44d9af4cb35855b44f35a91323ed9d65f81bdf9a
Glitch (ash-socket-template) <none>
2018-02-05 15:20:51 +0000

    🍲🏉 Checkpoint server.js:669240/143

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ io.on('connection', function(socket) {
   console.log('User connected');
   
   socket.on('message', function(message) {
+    message = message.trim();
+    if (message === '') return;
+    message = message.substring(0, 150);
     console.log('Message: ' + message);
     io.emit('message', message);
   });
