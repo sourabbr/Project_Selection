e05f803bd48cae4fc1c7a16f6995c0253911e33d
Glitch (healthy-turn) <none>
2018-02-05 13:22:44 +0000

    🎺📠 Checkpoint package.json:669240/56 public/client.js:669240/175 public/style.css:669240/3 views/index.html:669240/112 server.js:669240/665

--- public/client.js
+++ public/client.js
@@ -1,26 +1,13 @@
-// client-side js
-// run by the browser each time your view template is loaded
-
-// by default, you've got jQuery,
-// add other scripts at the bottom of index.html
-
 $(function() {
-  console.log('hello world :o');
   
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
+  var socket = io();
+  
   $('form').submit(function(event) {
     event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
+    var message = $('input').val();
+    socket.emit('message', message);
+    $('input').val('');
+    $('input').focus();
   });
 
 });
