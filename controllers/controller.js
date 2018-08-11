c6a74b72da9d0399b0b07cd81b49795bd7fd689b
Glitch (sel) <none>
2018-08-11 18:44:34 +0000

    🐌🎒 Checkpoint ./package.json:1534710/1076 ./controllers/socketcontroller.js:1534710/391 ./controllers/controller.js:1534710/659 ./server.js:1534710/93

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,27 @@
+const path = require("path");
 const low = require("lowdb");
 const FileAsync = require("lowdb/adapters/FileAsync");
 const adapter = new FileAsync('db.json');
-let controller = function (app) {
+let controller = function (app,io) {
 low(adapter)
     .then(db => {
   
       app.get("/", function (request, response) {
-        response.sendFile(__dirname + '/views/index.html');
+        response.sendFile(path.join(__dirname,'../views/index.html'));
       });
-  
+      
+      io.on('connection', function(socket) {
+        console.log('User connected');
+
+        socket.on('message', function(message) {
+          message = message.trim();
+          if (message === '') return;
+          message = message.substring(0, 150);
+
+          console.log('Message: ' + message);
+          io.emit('message', message);
+        });
+      });
   
   
       return db
--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const controller = require("./controllers/controller");
-const socketcontroller = require("./controllers/socketcontroller");
 app.use(express.static('public'));
-controller(app);
-socketcontroller(io);
+controller(app,io);
 const listener = server.listen(process.env.PORT, function () {
   console.log('Listening on port ' + listener.address().port);
 });
