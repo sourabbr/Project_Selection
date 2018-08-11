c046b097249f438d44c18c65a9539cd801a9614c
Glitch (sel) <none>
2018-08-11 19:38:18 +0000

    🙎🌓 Checkpoint ./server.js:1534710/1763 ./package.json:1534710/1069

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express')
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+
+const http = require('http')
+const express_force_ssl = require('express-force-ssl');
 const controller = require("./controllers/controller");
+
+
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
+
 app.use(express.static('public'));
+app.enable('trust proxy');
+ 
+app.use(express_force_ssl);
 controller(app,io);
 const listener = server.listen(process.env.PORT, function () {
   console.log('Listening on port ' + listener.address().port);
-})stener.address().port);
 });
 
