8dac00fcaa8e738607aef45e44e2eaed9679f36a
Glitch (selection) <none>
2018-09-03 12:25:49 +0000

    🍕🏥 Checkpoint ./.glitch-assets:1534710/446 ./controllers/importcontroller.js:1534710/43

--- controllers/importcontroller.js
+++ controllers/importcontroller.js
@@ -1,12 +1,12 @@
 var xlsx2json = require('xlsx2json');
 const import_xlsx = (response, db) => {
   xlsx2json(
-    'exports/registrations.xlsx',{
+    'imports/projects.xlsx',{
         sheet:0,
         dataStartingRow: 2,
         mapping: {
-            'title': 3,
-            'guide': 4
+            'title': 4,
+            'guide': 3
         }
     })
     .then(jsonArray => {
