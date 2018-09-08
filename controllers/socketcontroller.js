4c81654bb32dcb34354adca673acd56bba098f93
Glitch (ise-projectselection) <none>
2018-09-08 17:19:38 +0000

    🚐🚵 Checkpoint ./controllers/socketcontroller.js:1534710/5383

--- controllers/socketcontroller.js
+++ controllers/socketcontroller.js
@@ -49,82 +49,87 @@ let socketcontroller = (io, db) => {
   
 };
 const tryRegisterProject = async(projects,teamMembers,db,io,socket,headers) => { 
-          
-  let usnList = db.get("registeredUSNs").value();
-  for (let usn of teamMembers) {
-      if (usnList.includes(usn)) {
-          socket.emit('displayAlert', `${usn} already registered`, 'danger');
-          return;
-      }
-  }
-  var choices={
-    choice1:projects[0].title,guide1:projects[0].guide,
-    choice2:projects[1].title,guide2:projects[1].guide,
-    choice3:projects[2].title,guide3:projects[2].guide
-  }
-  var success=false;
-  for(var i=0;i<projects.length;i++)
-  {
-    var project=projects[i];
-    
-    if(success)
-      return;
-    
-    let existingRegistration = await db.get("registrations")
-                                      .find({title:project.title})
-                                      .value();
-    if(existingRegistration)
-      continue;
-    
-    let guide = await db.get("guides")
-                        .find({name:project.guide})
-                        .value();
-    
-    if(guide.registeredCount >= MAX_REGISTRATION_COUNT)
-      continue;
-
-    
-    await db.get("registrations")
-      .push({Timestamp: new Date().toLocaleString(),IP:headers['x-forwarded-for'],...project,...choices,teamMembers})
-      .write();
-     
-    await db.get("projects")
-      .find({title: project.title})
-      .assign({available: 0})
-      .write();
-    
-    await db.get("registeredUSNs")
-      .push(...teamMembers) 
-      .write();
-
-    guide.registeredCount+=1;
-    
-    await db.get("guides")
-      .find({name:project.guide})
-      .assign({registeredCount:guide.registeredCount})
-      .write();
-    
-    console.log(guide);
-    
-    if (guide.registeredCount == MAX_REGISTRATION_COUNT){
-      await db.get("projects")
-      .filter({guide:guide.name})
-      .each(proj=>{
-        proj.available=0;
-        io.emit('removeProject',proj);
-      })
-      .write();
-      console.log(guide.name+" done");
+  try{
+    let usnList = db.get("registeredUSNs").value();
+    for (let usn of teamMembers) {
+        if (usnList.includes(usn)) {
+            socket.emit('displayAlert', `${usn} already registered`, 'danger');
+            return;
+        }
     }
+    var choices={
+      choice1:projects[0].title,guide1:projects[0].guide,
+      choice2:projects[1].title,guide2:projects[1].guide,
+      choice3:projects[2].title,guide3:projects[2].guide
+    }
+    var success=false;
+    for(var i=0;i<projects.length;i++)
+    {
+      var project=projects[i];
+
+      if(success)
+        return;
+
+      let existingRegistration = await db.get("registrations")
+                                        .find({title:project.title})
+                                        .value();
+      if(existingRegistration)
+        continue;
+
+      let guide = await db.get("guides")
+                          .find({name:project.guide})
+                          .value();
+
+      if(guide.registeredCount >= MAX_REGISTRATION_COUNT)
+        continue;
+
+
+      await db.get("registrations")
+        .push({Timestamp: new Date().toLocaleString(),IP:headers['x-forwarded-for'],...project,...choices,teamMembers})
+        .write();
+
+      await db.get("projects")
+        .find({title: project.title})
+        .assign({available: 0})
+        .write();
+
+      await db.get("registeredUSNs")
+        .push(...teamMembers) 
+        .write();
+
+      guide.registeredCount+=1;
+
+      await db.get("guides")
+        .find({name:project.guide})
+        .assign({registeredCount:guide.registeredCount})
+        .write();
+
+      console.log(guide);
+
+      if (guide.registeredCount == MAX_REGISTRATION_COUNT){
+        await db.get("projects")
+        .filter({guide:guide.name})
+        .each(proj=>{
+          proj.available=0;
+          io.emit('removeProject',proj);
+        })
+        .write();
+        console.log(guide.name+" done");
+      }
+
+      io.emit('takenProject', {...project,teamMembers});
+      //io.to(`${socket.id}`).emit('successfullyRegistered',project.title);
+      socket.emit('successfullyRegistered',project.title);
+      success=true;
+      return;
+
+    }
+    socket.emit('displayAlert','Unfortunately, none of your choices are available, pick new set of choices','danger');
     
-    socket.broadcast.emit('takenProject', {...project,teamMembers});
-    //io.to(`${socket.id}`).emit('successfullyRegistered',project.title);
-    socket.emit('successfullyRegistered',project.title);
-    success=true;
-    return;
- 
+  }catch(e){
+    console.log(e);
   }
-  socket.emit('displayAlert','Unfortunately, none of your choices are available, pick new set of choices','danger');
+  
             
 }
 module.exports = socketcontroller;
